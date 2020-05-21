fc03058b31aea3e69b5e8e8782d78cfdfefd2831
Tu Nguyen <im2nguyen@users.noreply.github.com>
2020-05-20 21:07:57 -0700

    revert index.js

--- index.js
+++ index.js
@@ -1,16 +1,45 @@
 const build = require('./build')
-const mixinDeep = require('mixin-deep')
 
-function dearrayify(data) {
-  Object.keys(data).forEach((key) => {
-    if (Array.isArray(data[key]) && data[key].every(item => typeof item === 'object')) {
-      data[key] = data[key].reduce((mem, item) => {
-        return mixinDeep(mem, dearrayify(item))
-      }, {})
+const handleValue = (v) => {
+  try {
+    return JSON.parse(v)
+  } catch (e) {
+    return v
+  }
+}
+
+const attributeReshaper = (b) => {
+  const res = Object.keys(b.a || {}).reduce((acc, next) => ({
+    ...acc,
+    [next]: handleValue(b.a[next].s)
+  }), {})
+  return res
+}
+
+const bodyReshaper = (bodies) => {
+  const result = {}
+
+  for (let i = 0; i < bodies.length; i += 1) {
+    const node = bodies[i]
+    let ref = result
+    for (let j = 0; j < node.h.length; j += 1) {
+      const key = node.h[j]
+      if (!ref[key] && j < node.h.length - 1) {
+        ref[key] = {}
+      } else if (!ref[key]) {
+        ref[key] = attributeReshaper(node.b)
+        if (node.b && node.b.b) {
+          ref[key] = { ...ref[key], ...bodyReshaper(node.b.b) }
+        }
+      }
+      ref = ref[key]
     }
-  })
+  }
 
-  return data
+  console.log("-----")
+  console.log(result)
+
+  return result
 }
 
 function parseInput(input) {
@@ -20,9 +49,11 @@ function parseInput(input) {
   throw new TypeError('Expected input to be a string or buffer')
 }
 
-exports.parse = function parse(input) {
+const parse = (input) => {
   const src = parseInput(input)
   const [result, error] = build.parse(src)
+
+  console.log(result)
 
   if (error) {
     throw Object.assign(new Error('Malformed HCL'), {
@@ -32,5 +63,9 @@ exports.parse = function parse(input) {
     })
   }
 
-  return dearrayify(result)
+  return result
 }
+
+exports.parse = (input) => bodyReshaper(parse(input).r.b)
+
+exports.rawParse = (input) => parse(input)
