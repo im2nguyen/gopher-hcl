67a434d7feaf129e189edcd85e55676e65500bd9
Tu Nguyen <im2nguyen@users.noreply.github.com>
2020-05-20 21:04:36 -0700

    revert index.js

--- index.js
+++ index.js
@@ -1,52 +1,26 @@
 const build = require('./build')
+const mixinDeep = require('mixin-deep')
 
-const handleValue = (v) => {
-  try {
-    return JSON.parse(v)
-  } catch (e) {
-    return v
-  }
-}
-
-const attributeReshaper = (b) => {
-  const res = Object.keys(b.a || {}).reduce((acc, next) => ({
-    ...acc,
-    [next]: handleValue(b.a[next].s)
-  }), {})
-  return res
-}
-
-const bodyReshaper = (bodies) => {
-  const result = {}
-
-  for (let i = 0; i < bodies.length; i += 1) {
-    const node = bodies[i]
-    let ref = result
-    for (let j = 0; j < node.h.length; j += 1) {
-      const key = node.h[j]
-      if (!ref[key] && j < node.h.length - 1) {
-        ref[key] = {}
-      } else if (!ref[key]) {
-        ref[key] = attributeReshaper(node.b)
-        if (node.b && node.b.b) {
-          ref[key] = { ...ref[key], ...bodyReshaper(node.b.b) }
-        }
-      }
-      ref = ref[key]
+function dearrayify(data) {
+  Object.keys(data).forEach((key) => {
+    if (Array.isArray(data[key]) && data[key].every(item => typeof item === 'object')) {
+      data[key] = data[key].reduce((mem, item) => {
+        return mixinDeep(mem, dearrayify(item))
+      }, {})
     }
-  }
+  })
 
-  return result
+  return data
 }
 
-function parseInput (input) {
+function parseInput(input) {
   if (Buffer.isBuffer(input)) return new Uint8Array(input)
   if (typeof input === 'string') return new Uint8Array(Buffer.from(input))
 
   throw new TypeError('Expected input to be a string or buffer')
 }
 
-const parse = (input) => {
+exports.parse = function parse(input) {
   const src = parseInput(input)
   const [result, error] = build.parse(src)
 
@@ -58,9 +32,5 @@ const parse = (input) => {
     })
   }
 
-  return result
+  return dearrayify(result)
 }
-
-exports.parse = (input) => bodyReshaper(parse(input).r.b)
-
-exports.rawParse = (input) => parse(input)
