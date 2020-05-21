b803c6ad8e3410c89647b676f7a1cf7ed6c13363
Tu Nguyen <im2nguyen@users.noreply.github.com>
2020-05-21 11:40:36 -0700

    refactored to use hcl2, compiled buildjs

--- index.js
+++ index.js
@@ -1,47 +1,5 @@
 const build = require('./build')
 
-const handleValue = (v) => {
-  try {
-    return JSON.parse(v)
-  } catch (e) {
-    return v
-  }
-}
-
-const attributeReshaper = (b) => {
-  const res = Object.keys(b.a || {}).reduce((acc, next) => ({
-    ...acc,
-    [next]: handleValue(b.a[next].s)
-  }), {})
-  return res
-}
-
-const bodyReshaper = (bodies) => {
-  const result = {}
-
-  for (let i = 0; i < bodies.length; i += 1) {
-    const node = bodies[i]
-    let ref = result
-    for (let j = 0; j < node.h.length; j += 1) {
-      const key = node.h[j]
-      if (!ref[key] && j < node.h.length - 1) {
-        ref[key] = {}
-      } else if (!ref[key]) {
-        ref[key] = attributeReshaper(node.b)
-        if (node.b && node.b.b) {
-          ref[key] = { ...ref[key], ...bodyReshaper(node.b.b) }
-        }
-      }
-      ref = ref[key]
-    }
-  }
-
-  console.log("-----")
-  console.log(result)
-
-  return result
-}
-
 function parseInput(input) {
   if (Buffer.isBuffer(input)) return new Uint8Array(input)
   if (typeof input === 'string') return new Uint8Array(Buffer.from(input))
@@ -53,9 +11,8 @@ const parse = (input) => {
   const src = parseInput(input)
   const [result, error] = build.parse(src)
 
-  console.log(result)
-
   if (error) {
+    console.log(error)
     throw Object.assign(new Error('Malformed HCL'), {
       offset: error.Pos.Offset,
       line: error.Pos.Line,
@@ -66,6 +23,4 @@ const parse = (input) => {
   return result
 }
 
-exports.parse = (input) => bodyReshaper(parse(input).r.b)
-
-exports.rawParse = (input) => parse(input)
+exports.parse = (input) => parse(input)
